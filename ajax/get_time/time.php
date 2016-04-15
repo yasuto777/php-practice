a3a071ca923170aae7d55cc415ac225a8dbad264
相馬康人 <yasuto@YsBook.local>
2016-04-16 00:48:51 +0900

    check timestamp

--- ajax/get_time/time.php
+++ ajax/get_time/time.php
@@ -3,5 +3,6 @@ date_default_timezone_set('Asia/Tokyo');
     $timestamp = $_GET['val'];
 //echo $_GET['val'];
     echo date("Y/m/d H:i:s(D)",$timestamp);
-    echo "<br>".$timestamp;
+    echo "<br>JQuery timestamp:".$timestamp."<br>";
+    echo "PHP timestamp:".time();
 ?>
