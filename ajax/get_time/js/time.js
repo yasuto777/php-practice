0e273c3a818ed477a23ff0b4ebafacb2546b9eea
相馬康人 <yasuto@YsBook.local>
2016-04-16 00:04:47 +0900

    Move file

---
+++ ajax/get_time/js/time.js
@@ -0,0 +1,12 @@
+$(function(){
+    $('#btn').click(function(){
+        //クリック時にしたい処理
+        var timestamp = $.now();
+        //alert(timestamp);
+        $.get('time.php',{
+            val: timestamp
+        },function(data){
+            $("#output").html(data);
+        });
+    });
+});
