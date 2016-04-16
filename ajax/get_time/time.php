cc539f6b5f462d44e729024c5eef5c28ef3134bf
相馬康人 <yasuto@YsBook.local>
2016-04-16 12:51:41 +0900

    Add digit alignment

--- ajax/get_time/time.php
+++ ajax/get_time/time.php
@@ -4,7 +4,9 @@ date_default_timezone_set('Asia/Tokyo');
 //jsのタイムスタンプがミリ秒なので、無理くり合わせる
     $timestamp = floor($timestamp/1000);
 //echo $_GET['val'];
-    echo date("Y/m/d H:i:s(D)",$timestamp);
+    echo "<br>".date("Y/m/d H:i:s(D)",$timestamp);
+    /*
     echo "<br>JQuery timestamp:".$timestamp."<br>";
     echo "PHP timestamp:".time();
+    */
 ?>
