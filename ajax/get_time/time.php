19b7bb8f16aa2212a19446193e51e36677908458
相馬康人 <yasuto@YsBook.local>
2016-04-16 12:00:29 +0900

    Fix the time stamp of milliseconds to seconds

--- ajax/get_time/time.php
+++ ajax/get_time/time.php
@@ -1,6 +1,8 @@
 <?php
 date_default_timezone_set('Asia/Tokyo');
     $timestamp = $_GET['val'];
+//jsのタイムスタンプがミリ秒なので、無理くり合わせる
+    $timestamp = floor($timestamp/1000);
 //echo $_GET['val'];
     echo date("Y/m/d H:i:s(D)",$timestamp);
     echo "<br>JQuery timestamp:".$timestamp."<br>";
