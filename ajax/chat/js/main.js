bbbe5d0c5a662b80094b93ff8eb2eee7a95a0ae8
相馬康人 <yasuto@YsBook.local>
2016-04-17 21:33:57 +0900

    post user & message to php

--- ajax/chat/js/main.js
+++ ajax/chat/js/main.js
@@ -1,8 +1,20 @@
 $(function(){
     $('#send').click(function(){
-        //alert("Send!!");
         var user = $(':text[name="user"]').val();
         var message = $(':text[name="message"]').val();
-        //alert("user:"+user+"\n"+"message:"+message);
+        $.ajax({
+            type: 'POST',
+            url: "main.php",
+            dataType: 'html',
+            data: {
+                user: user,
+                message: message
+            },
+            success: function(data){
+                //送信成功時の処理
+                $('.outputField').append(data);
+            //$('.outputField').html(data);
+            }
+        });
     });
 });
