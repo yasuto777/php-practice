69ab3f4f6d2fa6b2ab8418f6ce188fedb267afbe
相馬康人 <yasuto@YsBook.local>
2016-04-17 22:44:53 +0900

    Add get time

--- ajax/chat/main.php
+++ ajax/chat/main.php
@@ -2,8 +2,10 @@
 
 $user = $_POST['user'];
 $message = $_POST['message'];
+$now = date("H:i");
 
-echo "<br><div class=\"arrow_name\">".$user."</div>";
+echo "<br><br><div class=\"arrow_name\">".$user."</div>";
     echo "<div class=\"arrow_box\">".$message."</div>";
+echo "<div class=\"send_time\">".$now."</div>";
 
 ?>
