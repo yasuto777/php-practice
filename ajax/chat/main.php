f92eebafa506f5a927e44483fdb95e1566d60989
相馬康人 <yasuto@YsBook.local>
2016-04-17 21:32:37 +0900

    Reply html

---
+++ ajax/chat/main.php
@@ -0,0 +1,9 @@
+<?php
+
+$user = $_POST['user'];
+$message = $_POST['message'];
+
+echo "<br><div class=\"arrow_name\">".$user."</div>";
+    echo "<div class=\"arrow_box\">".$message."</div>";
+
+?>
