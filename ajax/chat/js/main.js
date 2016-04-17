18caeddee6c157e840ac3a612bead7560f73825b
相馬康人 <yasuto@YsBook.local>
2016-04-17 23:19:14 +0900

    If form is blank, do not send

--- ajax/chat/js/main.js
+++ ajax/chat/js/main.js
@@ -1,20 +1,23 @@
-$(function(){
-    $('#send').click(function(){
+$(function () {
+    $('#send').click(function () {
+        //ボタンクリック時の処理
         var user = $(':text[name="user"]').val();
         var message = $(':text[name="message"]').val();
-        $.ajax({
-            type: 'POST',
-            url: "main.php",
-            dataType: 'html',
-            data: {
-                user: user,
-                message: message
-            },
-            success: function(data){
-                //送信成功時の処理
-                $('.outputField').append(data);
-                $('#message').val("");
-            }
-        });
+        if (user != "" && message != "") { //
+            $.ajax({
+                type: 'POST',
+                url: "main.php",
+                dataType: 'html',
+                data: {
+                    user: user,
+                    message: message
+                },
+                success: function (data) {
+                    //送信成功時の処理
+                    $('.outputField').append(data);
+                    $('#message').val("");
+                }
+            });
+        } //if end
     });
 });
