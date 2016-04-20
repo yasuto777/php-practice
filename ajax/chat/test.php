68fb54b267126c9fb89946c0e8107fb6dd927ece
yasuto777 <basilisk.0331@icloud.com>
2016-04-21 00:06:42 +0900

    Test to connect from php to MySQL

---
+++ ajax/chat/test.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+<title>PHPからMySQLに接続</title>
+<meta charset="utf-8">
+</head>
+<body>
+
+<?php
+//ここで処理が止まってる...
+//$mysqli = new mysqli("localhost","chatadmin","talk","chat");
+$mysqli = mysqli_connect("localhost","chatadmin","talk","chat");
+
+// Check connection
+if (mysqli_connect_errno()) {
+	printf("Connect failed: %s\n", mysqli_connect_error());
+	exit();
+}
+
+// Run SELECT query
+$query = "SELECT * FROM table1";
+
+if ($result = mysqli_query($mysqli,$query)){
+
+	// Get associative array
+	while($row = mysqli_fetch_assoc($result)){
+		printf("user:%s<br>message:%s<br><br>",$row["user"],$row["message"]);
+	}
+	//Free $result
+	mysql_free_result($result);
+}
+// Close connection
+mysqli_close($mysqli);
+?>
+
+</body>
+</html>
