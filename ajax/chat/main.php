5475b620bf0ee635bcb176b3529ae1864050d7c5
yasuto777 <basilisk.0331@icloud.com>
2016-04-21 16:08:21 +0900

    Insert $user,$message to MySQL

--- ajax/chat/main.php
+++ ajax/chat/main.php
@@ -2,12 +2,35 @@
 
 date_default_timezone_set('Asia/Tokyo');
 
+/*####### MySQL #########*/
+$host = "localhost";
+$dbuser = "chatadmin";
+$dbpass = "talk";
+$db = "chat";
+// Connect to MySQL
+$mysqli = mysqli_connect($host,$dbuser,$dbpass,$db);
+
+// Check connection<
+if (mysqli_connect_errno()) {
+printf("Connect failed: %s\n", mysqli_connect_error());
+exit();
+}
+
 $user = $_POST['user'];
 $message = $_POST['message'];
-$now = date("H:i");
+$now = date("Y-m-d H:i:s");
+
+//Send query to MySQL
+if(mysqli_query($mysqli,"INSERT INTO table1 (user,message) VALUES('$user','$message')") === TRUE){
 
 echo "<br><br><div class=\"arrow_name\">".$user."</div>";
     echo "<div class=\"arrow_box\">".$message."</div>";
 echo "<div class=\"send_time\">".$now."</div>";
+
+//Free $result
+//mysql_free_result($result);
+}
+//Close connecton
+mysqli_close($mysqli);
 
 ?>
